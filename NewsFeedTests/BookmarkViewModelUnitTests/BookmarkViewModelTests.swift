255711407978ea48327beeadcb4a17d90422c5bf
Abhishek Kohli <abhishekkohli2012@gmail.com>
2024-12-04 22:20:38 +0530

    Implemmented Unit Test Case

--- NewsFeedTests/BookmarkViewModelUnitTests/BookmarkViewModelTests.swift
+++ NewsFeedTests/BookmarkViewModelUnitTests/BookmarkViewModelTests.swift
@@ -5,4 +5,39 @@
 //  Created by Abhishek Kohli on 04/12/24.
 //
 
-import Foundation
+import XCTest
+import SwiftUI
+@testable import NewsFeed
+
+class BookmarkViewModelTests: XCTestCase {
+    
+    var viewModel: BookmarkViewModel!
+    var mockArticle: Article!
+    
+    override func setUpWithError() throws {
+        super.setUp()
+        mockArticle =  Article(
+            source: Source(id: "source1", name: "Source 1"),
+            author: "Author 1",
+            title: "Mock Article 1",
+            description: "This is the description for mock article 1",
+            url: "https://www.example.com/mock-article-1",
+            urlToImage: "https://www.example.com/image1.jpg",
+            publishedAt: "2024-12-04T12:00:00Z",
+            content: "Content of the mock article 1",
+            isBookmarked: false
+        )
+        
+        viewModel = BookmarkViewModel()
+    }
+    
+    override func tearDownWithError() throws {
+        viewModel = nil
+        mockArticle = nil
+        super.tearDown()
+    }
+    
+    func testLoadBookmarkedArticles_whenNoBookmarks_shouldReturnEmptyArray() {
+        XCTAssertTrue(viewModel.bookmarkedArticles.isEmpty)
+    }
+}
